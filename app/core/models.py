5d19fd8d67d0dee6925e368a9a3777bbbf811184
Daniel Lund <Dan@Daniels-MacBook-Air.local>
2021-03-26 09:07:00 +0000

    Added endpoint for creating users and generating auth tokens.

--- app/core/models.py
+++ app/core/models.py
@@ -6,9 +6,9 @@ from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, \
 class UserManager(BaseUserManager):
 
     def create_user(self, email, password=None, **extra_fields):
-        """creates and saves a new user"""
+        """Creates and saves a new user"""
         if not email:
-            raise ValueError('Users must have an email address.')
+            raise ValueError('Users must have an email address')
         user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
@@ -16,7 +16,7 @@ class UserManager(BaseUserManager):
         return user
 
     def create_superuser(self, email, password):
-        """Creates and saves a new superuser"""
+        """Creates and saves a new super user"""
         user = self.create_user(email, password)
         user.is_staff = True
         user.is_superuser = True
@@ -26,7 +26,7 @@ class UserManager(BaseUserManager):
 
 
 class User(AbstractBaseUser, PermissionsMixin):
-    """custom user model that supports using email instead of username"""
+    """Custom user model that suppors using email instead of username"""
     email = models.EmailField(max_length=255, unique=True)
     name = models.CharField(max_length=255)
     is_active = models.BooleanField(default=True)
